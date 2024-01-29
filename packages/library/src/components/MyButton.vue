563dc9964b334c784d2e6046643a03bb02be7f76
Daniel Kelly <me@danielkelly.io>
2024-01-29 15:52:49 -0600

    init commit

---
+++ packages/library/src/components/MyButton.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineProps<{
+  label?: string
+}>()
+</script>
+<template>
+  <button class="my-button">
+    <slot>{{ label }}</slot>
+  </button>
+</template>
+
+<style scoped>
+.my-button {
+  background-color: #f00;
+}
+</style>
