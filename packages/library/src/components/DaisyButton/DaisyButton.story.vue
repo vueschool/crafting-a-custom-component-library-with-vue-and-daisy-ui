c9cc9ba7a3b7b61e123bfb8594e34115db477d6e
Daniel Kelly <me@danielkelly.io>
2024-02-01 09:58:30 -0600

    wip

--- packages/library/src/components/DaisyButton/DaisyButton.story.vue
+++ packages/library/src/components/DaisyButton/DaisyButton.story.vue
@@ -1,8 +1,18 @@
 <script setup lang="ts">
+import { reactive } from 'vue'
 import DaisyButton from './DaisyButton.vue'
+
+const state = reactive({
+  content: 'Click Me',
+  disabled: false
+})
 </script>
 <template>
   <Story>
-    <DaisyButton>Click Me</DaisyButton>
+    <DaisyButton :disabled="state.disabled">{{ state.content }}</DaisyButton>
+    <template #controls>
+      <HstText v-model="state.content" title="Content" />
+      <HstCheckbox v-model="state.disabled" title="Disabled" />
+    </template>
   </Story>
 </template>
--- packages/library/src/components/DaisyButton/DaisyButton.vue
+++ packages/library/src/components/DaisyButton/DaisyButton.vue
@@ -1,10 +1,57 @@
 <script setup lang="ts">
-defineProps<{
-  label?: string
-}>()
+import { computed, ref } from 'vue'
+const props = withDefaults(
+  defineProps<{
+    color:
+      | 'neutral'
+      | 'primary'
+      | 'secondary'
+      | 'accent'
+      | 'error'
+      | 'warning'
+      | 'success'
+      | 'info'
+      | 'ghost'
+      | 'link'
+
+    size: 'lg' | 'default' | 'sm' | 'xs'
+    wide?: boolean
+    outline?: boolean
+  }>(),
+  {
+    color: 'neutral',
+    size: 'default',
+    wide: false,
+    outline: false
+  }
+)
+
+const classes = computed(() => {
+  return {
+    // colors
+    'btn-neutral': props.color === 'neutral',
+    'btn-primary': props.color === 'primary',
+    'btn-secondary': props.color === 'secondary',
+    'btn-accent': props.color === 'accent',
+    'btn-error': props.color === 'error',
+    'btn-warning': props.color === 'warning',
+    'btn-success': props.color === 'success',
+    'btn-info': props.color === 'info',
+    'btn-ghost': props.color === 'ghost',
+    'btn-link': props.color === 'link',
+    // size
+    'btn-lg': props.size === 'lg',
+    'btn-sm': props.size === 'sm',
+    'btn-xs': props.size === 'xs',
+
+    // outline
+    'btn-outline': props.outline,
+    'btn-wide': props.wide
+  }
+})
 </script>
 <template>
-  <button class="btn-primary btn">
-    <slot>{{ label }}</slot>
+  <button class="btn" :class="classes">
+    <slot></slot>
   </button>
 </template>
