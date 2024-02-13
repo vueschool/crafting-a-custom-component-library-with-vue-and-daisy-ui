1d78ceff88d80821a6080d2a0b6e9bd1a1bd4d39
Daniel Kelly <me@danielkelly.io>
2024-02-13 12:00:03 -0600

    feature: steps component

---
+++ packages/library/src/components/DaisySteps/DaisySteps.story.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { reactive } from 'vue'
+import DaisySteps from './DaisySteps.vue'
+
+const state = reactive({
+  activeStep: 1
+})
+</script>
+<template>
+  <Story :layout="{ type: 'grid', width: '100%' }">
+    <Variant title="Horizontal">
+      <DaisySteps
+        :steps="['Go to store', 'Get Milk', 'Get Bread', 'Checkout at Register']"
+        v-model:active-step="state.activeStep"
+      />
+    </Variant>
+
+    <Variant title="Vertical">
+      <DaisySteps
+        :steps="['Go to store', 'Get Milk', 'Get Bread', 'Checkout at Register']"
+        layout="vertical"
+        v-model:active-step="state.activeStep"
+      />
+    </Variant>
+
+    <Variant title="Responsive">
+      <DaisySteps
+        :steps="['Go to store', 'Get Milk', 'Get Bread', 'Checkout at Register']"
+        layout="responsive"
+        v-model:active-step="state.activeStep"
+      />
+    </Variant>
+
+    <template #controls> </template>
+  </Story>
+</template>
---
+++ packages/library/src/components/DaisySteps/DaisySteps.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+const props = withDefaults(
+  defineProps<{
+    steps: string[]
+    layout?: 'horizontal' | 'vertical' | 'responsive'
+  }>(),
+  {
+    layout: 'horizontal'
+  }
+)
+
+const activeStep = defineModel<number>('activeStep', {
+  default: 1
+})
+</script>
+<template>
+  <ul
+    class="steps"
+    :class="{
+      'steps-horizontal': props.layout === 'horizontal',
+      'steps-vertical': props.layout === 'vertical',
+      'steps-vertical lg:steps-horizontal': props.layout === 'responsive'
+    }"
+  >
+    <li
+      v-for="(step, index) in steps"
+      @click="activeStep = index + 1"
+      :class="{
+        'step-primary': index + 1 <= activeStep
+      }"
+      :key="step"
+      class="step"
+    >
+      {{ step }}
+    </li>
+  </ul>
+</template>
