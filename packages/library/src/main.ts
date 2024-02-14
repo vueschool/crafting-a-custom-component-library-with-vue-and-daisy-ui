05884a5a248f788847679c85dba43f4681a0d5d9
Daniel Kelly <me@danielkelly.io>
2024-02-14 10:28:46 -0600

    docs: wip

--- packages/library/src/main.ts
+++ packages/library/src/main.ts
@@ -1,4 +1,7 @@
 import DaisyPopover from './components/DaisyPopover/DaisyPopover.vue'
 import DaisyButton from './components/DaisyButton/DaisyButton.vue'
+import DaisyAlert from './components/DaisyAlert/DaisyAlert.vue'
 
-export { DaisyPopover, DaisyButton }
+export { DaisyPopover, DaisyButton, DaisyAlert }
+
+export default { DaisyPopover, DaisyButton, DaisyAlert }
--- packages/library/vite.config.ts
+++ packages/library/vite.config.ts
@@ -1,16 +1,35 @@
 import { fileURLToPath, URL } from 'node:url'
+import { resolve } from 'node:path'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
+    dedupe: ['vue'],
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/main.ts'),
+      name: 'DaisyVue',
+      fileName: 'lib'
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue'
+        }
+      }
     }
   }
 })
