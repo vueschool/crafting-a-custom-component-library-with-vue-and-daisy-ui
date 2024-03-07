9b5f9e7ba66c4099f4caf8152391614db89c5ee9
Daniel Kelly <me@danielkelly.io>
2024-03-07 15:30:39 -0600

    feature: global component types

--- packages/library/src/main.ts
+++ packages/library/src/main.ts
@@ -5,6 +5,11 @@ import * as components from '././components/components'
 import { setConfig } from './global-config'
 import type { Config } from './global-config'
 
+type Components = typeof components
+declare module 'vue' {
+  interface GlobalComponents extends Components {}
+}
+
 export function createDaisyVue(config?: Config) {
   const plugin = {
     // need this for the Type to be right even if we aren't using it
