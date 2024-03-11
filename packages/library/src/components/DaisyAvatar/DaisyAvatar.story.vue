e195e97b110b999cbf4329f2495531f5fb18bf79
Daniel Kelly <me@danielkelly.io>
2024-03-11 14:51:33 -0500

    example of badge with avatar

--- packages/library/src/components/DaisyAvatar/DaisyAvatar.story.vue
+++ packages/library/src/components/DaisyAvatar/DaisyAvatar.story.vue
@@ -1,23 +1,26 @@
 <script setup lang="ts">
 import { reactive } from 'vue'
 import DaisyAvatar from './DaisyAvatar.vue'
-import DaisyAvatarGroup from '../DaisyAvatarGroup/DaisyAvatarGroup.vue'
-import { sizes } from '@/globals'
+import DaisyBadge from '../DaisyBadge/DaisyBadge.vue'
+import { sizes, masks, Masks } from '@/globals'
 
-const state = reactive({})
+const state = reactive({
+  mask: 'circle' as Masks
+})
 </script>
 <template>
   <Story :layout="{ type: 'grid', width: '25%' }">
     <Variant :title="`With Image - ${size}`" v-for="size in sizes" :key="size">
-      <DaisyAvatar :size="size" :src="`https://i.pravatar.cc/300?u=2`" />
+      <DaisyAvatar :mask="state.mask" :size="size" :src="`https://i.pravatar.cc/300?u=2`" />
     </Variant>
 
     <Variant :title="`With Placeholder - ${size}`" v-for="size in sizes" :key="size">
-      <DaisyAvatar :size="size" placeholder="DK" class="bg-red-500" />
+      <DaisyAvatar :mask="state.mask" :size="size" placeholder="DK" class="bg-red-500" />
     </Variant>
 
     <Variant :title="`Custom BG Color ${size}`" v-for="size in sizes" :key="size">
       <DaisyAvatar
+        :mask="state.mask"
         :size="size"
         placeholder="DK"
         :class="
@@ -33,6 +36,7 @@ const state = reactive({})
 
     <Variant :title="`Custom BG Color ${size}`" v-for="size in sizes" :key="size">
       <DaisyAvatar
+        :mask="state.mask"
         :size="size"
         placeholder="DK"
         :class="
@@ -46,6 +50,27 @@ const state = reactive({})
       />
     </Variant>
 
-    <template #controls></template>
+    <Variant :title="`With Empty Badge - ${size}`" v-for="size in sizes" :key="size">
+      <div class="relative inline-block">
+        <DaisyBadge class="absolute top-0 right-0 z-10" color="success" :size="size"></DaisyBadge>
+        <DaisyAvatar :mask="state.mask" :size="size" :src="`https://i.pravatar.cc/300?u=2`" />
+      </div>
+    </Variant>
+
+    <Variant :title="`With Badge - ${size}`" v-for="size in sizes" :key="size">
+      <div class="relative inline-block">
+        <DaisyBadge
+          class="absolute top-0 z-10 left-full translate-x-[-15px]"
+          color="success"
+          :size="size"
+          >99+</DaisyBadge
+        >
+        <DaisyAvatar :mask="state.mask" :size="size" :src="`https://i.pravatar.cc/300?u=2`" />
+      </div>
+    </Variant>
+
+    <template #controls>
+      <HstSelect v-model="state.mask" :options="masks" title="Mask" />
+    </template>
   </Story>
 </template>
--- packages/library/src/components/DaisyAvatarGroup/DaisyAvatarGroup.story.vue
+++ packages/library/src/components/DaisyAvatarGroup/DaisyAvatarGroup.story.vue
@@ -2,26 +2,28 @@
 import { reactive } from 'vue'
 import DaisyAvatar from '../DaisyAvatar/DaisyAvatar.vue'
 import DaisyAvatarGroup from './DaisyAvatarGroup.vue'
-import { sizes } from '@/globals'
+import { sizes, masks, Masks } from '@/globals'
 
 const state = reactive({
-  size: 'sm'
+  size: 'sm',
+  mask: 'circle' as Masks
 })
 </script>
 <template>
   <Story :layout="{ type: 'grid', width: '100%' }">
     <Variant title="Variant Title">
       <DaisyAvatarGroup :size="state.size">
-        <DaisyAvatar src="https://i.pravatar.cc/300?u=1"></DaisyAvatar>
-        <DaisyAvatar placeholder="DK" class="bg-red-600"></DaisyAvatar>
-        <DaisyAvatar src="https://i.pravatar.cc/300?u=3"></DaisyAvatar>
-        <DaisyAvatar src="https://i.pravatar.cc/300?u=4"></DaisyAvatar>
-        <DaisyAvatar placeholder="99+" class="bg-gray-500"></DaisyAvatar>
+        <DaisyAvatar :mask="state.mask" src="https://i.pravatar.cc/300?u=1"></DaisyAvatar>
+        <DaisyAvatar :mask="state.mask" placeholder="DK" class="bg-red-600"></DaisyAvatar>
+        <DaisyAvatar :mask="state.mask" src="https://i.pravatar.cc/300?u=3"></DaisyAvatar>
+        <DaisyAvatar :mask="state.mask" src="https://i.pravatar.cc/300?u=4"></DaisyAvatar>
+        <DaisyAvatar :mask="state.mask" placeholder="99+" class="bg-gray-500"></DaisyAvatar>
       </DaisyAvatarGroup>
     </Variant>
 
     <template #controls>
-      <HstSelect v-model="state.size" :options="sizes"></HstSelect>
+      <HstSelect v-model="state.size" :options="sizes" title="Size"></HstSelect>
+      <HstSelect v-model="state.mask" :options="masks" title="Mask"></HstSelect>
     </template>
   </Story>
 </template>
