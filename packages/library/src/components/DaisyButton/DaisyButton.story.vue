102cf07d2474e4fef7f51b5aef6da3ddf000c408
Daniel Kelly <me@danielkelly.io>
2024-03-12 13:30:44 -0500

    9-button-component-challenge

--- packages/library/src/components/DaisyButton/DaisyButton.story.vue
+++ packages/library/src/components/DaisyButton/DaisyButton.story.vue
@@ -1,24 +1,44 @@
+// DaisyButton.story.vue
 <script setup lang="ts">
 import { reactive } from 'vue'
 import DaisyButton from './DaisyButton.vue'
-import { colorsBrand, colorsState } from '../../globals'
+import { sizes, Sizes, colorsBrand, colorsState } from '../../globals'
 
 const state = reactive({
-  disabled: false
+  content: 'Click Me',
+  disabled: false,
+  wide: false,
+  size: 'md' as Sizes,
+  circle: false,
+  square: false,
+  loading: false,
+  outline: false,
+  glass: false,
+  active: false
 })
+
+const colors = [...colorsBrand, ...colorsState, 'ghost', 'link'] as const
 </script>
 <template>
-  <Story :layout="{ type: 'grid', width: '25%' }">
-    <Variant
-      v-for="color in [...colorsBrand, ...colorsState, 'ghost' as const, 'link' as const]"
-      :key="color"
-      :title="color"
-    >
-      <DaisyButton :color="color" :disabled="state.disabled"> Click Me </DaisyButton>
+  <Story :layout="{ type: 'grid', width: '50%' }">
+    <Variant title="No Color">
+      <DaisyButton v-bind="state">{{ state.content }}</DaisyButton>
+    </Variant>
+    <Variant v-for="color in colors" :key="color" :title="color">
+      <DaisyButton :color="color" v-bind="state">{{ state.content }}</DaisyButton>
     </Variant>
 
     <template #controls>
+      <HstText v-model="state.content" title="Content" />
       <HstCheckbox v-model="state.disabled" title="Disabled" />
+      <HstCheckbox v-model="state.active" title="Active" />
+      <HstCheckbox v-model="state.circle" title="Circle" />
+      <HstCheckbox v-model="state.square" title="Square" />
+      <HstCheckbox v-model="state.wide" title="Wide" />
+      <HstCheckbox v-model="state.loading" title="Loading" />
+      <HstCheckbox v-model="state.outline" title="Outline" />
+      <HstCheckbox v-model="state.glass" title="Glass" />
+      <HstSelect v-model="state.size" :options="sizes" title="Size" />
     </template>
   </Story>
 </template>
--- packages/library/src/components/DaisyButton/DaisyButton.vue
+++ packages/library/src/components/DaisyButton/DaisyButton.vue
@@ -1,26 +1,65 @@
+// DaisyButton.vue
 <script setup lang="ts">
 import { computed } from 'vue'
-import { ColorsBrand, ColorsState } from '../../globals'
-const props = defineProps<{
-  color: ColorsBrand | ColorsState | 'ghost' | 'link'
-}>()
+import DaisyLoading from '../DaisyLoading/DaisyLoading.vue'
+import type { Sizes, ColorsBrand, ColorsState } from '../../globals'
 
-const colorClass = computed(() => {
+const props = withDefaults(
+  defineProps<{
+    color?: ColorsBrand | ColorsState | 'ghost' | 'link'
+    size?: Sizes
+    wide?: boolean
+    outline?: boolean
+    square?: boolean
+    circle?: boolean
+    loading?: boolean
+    glass?: boolean
+    active: boolean
+  }>(),
+  {
+    size: 'md',
+    wide: false,
+    outline: false
+  }
+)
+
+const colorClasses = computed(() => {
   return {
     neutral: 'btn-neutral',
     primary: 'btn-primary',
     secondary: 'btn-secondary',
     accent: 'btn-accent',
-    ghost: 'btn-ghost',
-    success: 'btn-success',
-    warning: 'btn-warning',
     error: 'btn-error',
+    warning: 'btn-warning',
+    success: 'btn-success',
     info: 'btn-info',
+    ghost: 'btn-ghost',
     link: 'btn-link'
   }[props.color]
 })
-</script>
 
+const sizeClasses = computed(() => {
+  return {
+    lg: 'btn-lg',
+    sm: 'btn-sm',
+    xs: 'btn-xs'
+  }[props.size]
+})
+
+const otherClasses = computed(() => {
+  return {
+    'btn-outline': props.outline,
+    'btn-wide': props.wide,
+    'btn-square': props.square,
+    'btn-circle': props.circle,
+    glass: props.glass,
+    'btn-active': props.active
+  }
+})
+</script>
 <template>
-  <button class="btn" :class="colorClass"><slot></slot></button>
+  <button class="btn" :class="[colorClasses, sizeClasses, otherClasses]">
+    <DaisyLoading v-if="loading" :size="size" />
+    <slot></slot>
+  </button>
 </template>
