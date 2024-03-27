349be1651b22f53f11e7a2a4108d01246425524e
Daniel Kelly <me@danielkelly.io>
2024-03-27 16:34:44 -0500

    wip

--- packages/library/src/components/DaisyRating/DaisyRating.vue
+++ packages/library/src/components/DaisyRating/DaisyRating.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import type { DirectiveValueType as Shapes } from '@/directives/vMask/VMask'
+import type { DirectiveValueType as Shapes } from '../../directives/vMask/VMask'
 import { vMask } from '../../directives/vMask/VMask'
 withDefaults(
   defineProps<{
