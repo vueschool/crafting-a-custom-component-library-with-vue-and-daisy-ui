641b899f942d3796dc76e5a4bb99cd7731cc22ee
Daniel Kelly <me@danielkelly.io>
2024-03-27 17:12:53 -0500

    feature: vmask half modifier

--- packages/library/src/directives/vMask/VMask.ts
+++ packages/library/src/directives/vMask/VMask.ts
@@ -17,17 +17,23 @@ function addMaskClasses(vnode: VNode, binding: DirectiveBinding) {
   vnode.props = vnode.props || {}
   vnode.props.class = vnode.props.class || ''
 
+  const validMods = ['half-1', 'half-2']
+
   if (typeof vnode.props.class === 'string') {
     vnode.props.class = vnode.props.class
       .split(' ')
       .filter((c) => !c.startsWith('mask-') || c === 'mask')
       .join(' ')
-    vnode.props.class += ` mask mask-${binding.value}`
+    const modifer = validMods.includes(binding.arg) ? `mask-${binding.arg}` : ''
+    vnode.props.class += ` mask mask-${binding.value} ${modifer}`
   }
   if (Array.isArray(vnode.props.class)) {
     vnode.props.class = vnode.props.class.filter((c) => !c.startsWith('mask-') || c === 'mask')
     vnode.props.class.push(`mask-${binding.value}`)
     vnode.props.class.push(`mask`)
+    if (validMods.includes(binding.arg)) {
+      vnode.props.class.push(`mask-${binding.arg}`)
+    }
   }
 
   if (isPlainObject(vnode.props.class)) {
@@ -36,6 +42,12 @@ function addMaskClasses(vnode: VNode, binding: DirectiveBinding) {
     })
     vnode.props.class.mask = true
     vnode.props.class[`mask-${binding.value}`] = true
+    if (binding.arg === 'half-1') {
+      vnode.props.class['mask-half-1'] = true
+    }
+    if (binding.arg === 'half-2') {
+      vnode.props.class['mask-half-2'] = true
+    }
   }
 }
 
