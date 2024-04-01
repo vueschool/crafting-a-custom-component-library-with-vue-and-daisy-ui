e1598830491b27544fdaefb6bf2319f36983d060
Daniel Kelly <me@danielkelly.io>
2024-04-01 14:23:29 -0500

    fix: document undefined in docs

--- packages/docs/.vitepress/theme/theme-switcher-support.ts
+++ packages/docs/.vitepress/theme/theme-switcher-support.ts
@@ -1,29 +1,31 @@
-const targetNode = document.querySelector('html') as HTMLElement
-setTheme()
+if (document) {
+  const targetNode = document.querySelector('html') as HTMLElement
+  setTheme()
 
-// Options for the observer (which mutations to observe)
-const config = { attributes: true }
+  // Options for the observer (which mutations to observe)
+  const config = { attributes: true }
 
-// Callback function to execute when mutations are observed
-const callback = (mutationList, observer) => {
-  for (const mutation of mutationList) {
-    if (mutation.attributeName === 'class') {
-      setTheme()
+  // Callback function to execute when mutations are observed
+  const callback = (mutationList, observer) => {
+    for (const mutation of mutationList) {
+      if (mutation.attributeName === 'class') {
+        setTheme()
+      }
     }
   }
-}
 
-function setTheme() {
-  const isDark = targetNode.classList.contains('dark')
-  if (isDark) {
-    targetNode.setAttribute('data-theme', 'dark')
-  } else {
-    targetNode.setAttribute('data-theme', 'light')
+  function setTheme() {
+    const isDark = targetNode.classList.contains('dark')
+    if (isDark) {
+      targetNode.setAttribute('data-theme', 'dark')
+    } else {
+      targetNode.setAttribute('data-theme', 'light')
+    }
   }
+
+  // Create an observer instance linked to the callback function
+  const observer = new MutationObserver(callback)
+
+  // Start observing the target node for configured mutations
+  observer.observe(targetNode, config)
 }
-
-// Create an observer instance linked to the callback function
-const observer = new MutationObserver(callback)
-
-// Start observing the target node for configured mutations
-observer.observe(targetNode, config)
