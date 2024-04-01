52154cd581329cfefd9649480a0593140d38a100
Daniel Kelly <me@danielkelly.io>
2024-04-01 15:03:24 -0500

    fix: docs template switcher

--- packages/docs/.vitepress/theme/index.ts
+++ packages/docs/.vitepress/theme/index.ts
@@ -1,20 +1,26 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from 'vue'
+import { h, defineComponent, onMounted } from 'vue'
 import type { Theme } from 'vitepress'
-import './theme-switcher-support'
+import { initThemeSwitcher } from './theme-switcher-support'
 
-// import PlaygroundWrapper from '../components/PlaygroundWrapper.vue'
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
 
 export default {
   extends: DefaultTheme,
   Layout: () => {
-    return h(DefaultTheme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    })
+    return h(
+      defineComponent({
+        setup() {
+          onMounted(() => {
+            initThemeSwitcher()
+          })
+          return () =>
+            h(DefaultTheme.Layout, null, {
+              // https://vitepress.dev/guide/extending-default-theme#layout-slots
+            })
+        }
+      })
+    )
   }
-  // enhanceApp({ app, router, siteData }) {
-  //   app.component('VuePlayground', PlaygroundWrapper)
-  // }
 } satisfies Theme
